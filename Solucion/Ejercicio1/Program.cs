7560289c70b4e1f05fcd8230a0495f80b1389085
Matias Alvarez <matiasealvarez89@gmail.com>
2022-08-22 21:19:13 -0300

    first commit

---
+++ Solucion/Ejercicio1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
