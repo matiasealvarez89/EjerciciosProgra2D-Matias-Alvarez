7163d95b7ad4b7225fdc78cd60098f4c5d5cfff4
Matias Alvarez <matiasealvarez89@gmail.com>
2022-08-22 22:25:26 -0300

    primer clase

--- Solucion/Ejercicio1/Program.cs
+++ Solucion/Ejercicio1/Program.cs
@@ -6,7 +6,33 @@ namespace Ejercicio1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numeroIngresado;
+            int minimo = int.MaxValue;
+            int maximo = int.MinValue;
+            
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Ingrese un numero");
+                numeroIngresado = int.Parse(Console.ReadLine());
+
+                if(i == 0)
+                {
+                    minimo = numeroIngresado;
+                    maximo = numeroIngresado;                    
+                }
+
+                if(numeroIngresado < minimo)
+                {
+                    minimo = numeroIngresado;
+                }else if(numeroIngresado > maximo)
+                {
+                    maximo = numeroIngresado;
+                }
+            }
+
+            Console.WriteLine($"El minimo es: {minimo} y el maximo es {maximo}");
+
         }
     }
 }
---
+++ Solucion/Ejercicio2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroIngresado;
+            int estaBien = 0;
+            do
+            {
+                Console.WriteLine("Ingrese un numero");
+                int.TryParse(Console.ReadLine(), out numeroIngresado);
+                if (numeroIngresado <= 0)
+                {
+                    Console.WriteLine("ERROR. ¡Reingresar número!");
+                }
+                else
+                {
+                    Console.WriteLine($"El cuadrado es {numeroIngresado * numeroIngresado} y el cubo es {numeroIngresado * numeroIngresado * numeroIngresado}");
+                    estaBien = 1;
+                }
+            } while (estaBien == 0);
+        }
+    }
+}
